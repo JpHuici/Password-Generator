0925ed8b96fa613dc22e09a0adf8e9cd7196e32f
Juan Pablo Huici <jphuici1@gmail.com>
2023-02-06 16:52:16 -0300

    navbar added, logic to home, password and about, base added

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from generator import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-    path('about/', views.about),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
     path('password/', views.password, name='password'),
 ]
